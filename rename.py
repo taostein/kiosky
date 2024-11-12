22db2d0f51bbfc16ef430825030303d6dd28a264
Tao Stein <taostein@gmail.com>
2024-11-12 22:27:33 +0800

    Commit

--- rename.py
+++ rename.py
@@ -3,13 +3,13 @@
 import re
 import os
 files = os.listdir() 
-regex = '\\[([A-Za-z0-9_\\-]+)\\]\\.webm$'
-# regex = re.escape('[([A-Za-z0-9_-]+)].webm$')
+regex = '\\[([A-Za-z0-9_\\-]{11})\\]\\.(webm|mp4)$'
 for file in files:
     match = re.search(regex, file)
     if match != None:
         key = match.group(1)
+        suffix = match.group(2)
         file = file.replace("'", re.escape("'\"'\"'"))
-        cmd = "cp '" + file + "' " + key + ".webm"
+        cmd = "cp '" + file + "' " + key + "." + suffix
         print(cmd)
 
