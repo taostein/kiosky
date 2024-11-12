c8473c788275b78dd1fe2a6a4088f1a2febf263b
Tao Stein <taostein@gmail.com>
2024-11-12 22:27:42 +0800

    Commit

--- extract-audio.py
+++ extract-audio.py
@@ -2,7 +2,7 @@
 import re
 import os
 files = os.listdir()
-regex = '^([A-Za-z0-9_\\-]{11}).webm$'
+regex = '^([A-Za-z0-9_\\-]{11}).(webm|mp4)$'
 for file in files:
     match = re.match(regex, file)
     if match != None:
